713124c0242a94fc81f1e8ed2e161e4aeb7725df
jimmy-martin <jimmy.martin952@gmail.com>
2021-12-09 13:48:19 +0100

    feat update authentication success datas

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -2,7 +2,7 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 
 class AuthenticationSuccessListener
@@ -15,13 +15,25 @@ class AuthenticationSuccessListener
         $data = $event->getData();
         $user = $event->getUser();
 
-        if (!$user instanceof UserInterface) {
+        if (!$user instanceof User) {
             return;
         }
 
         $data['data'] = [
             'user' => [
                 'id' => $user->getId(),
+                'email' => $user->getEmail(),
+                'roles' => $user->getRoles(),
+                'firstname' => $user->getFirstname(),
+                'lastname' => $user->getLastname(),
+                'phoneNumber' => $user->getPhoneNumber(),
+                'avatar' => $user->getAvatar(),
+                'address' => $user->getAddress(),
+                'zipcode' => $user->getZipcode(),
+                'city' => $user->getCity(),
+                'country' => $user->getCountry(),
+                'latitude' => $user->getLatitude(),
+                'longitude' => $user->getLongitude(),                
             ]
         ];
 
